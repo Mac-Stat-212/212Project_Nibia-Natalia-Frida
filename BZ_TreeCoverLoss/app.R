473f5061b04b7a8a2ef9765a4bbca1dd5a78ef36
nbs2809 <nbecerra@macalester.edu>
2025-04-28 09:30:48 -0500

    Latest working shiny with issues and code still messy

--- BZ_TreeCoverLoss/app.R
+++ BZ_TreeCoverLoss/app.R
@@ -42,15 +42,14 @@ state_nameBR <- states %>% pull(name_state) %>% unique()
 # Making loss over time
 
 tree_cover_loss_brazil <- tree_cover_loss  %>% 
+  filter(country == "Brazil", threshold == 30) %>%
+  select(-country, -threshold) %>%
   mutate(
     loss_2000.2020_ha = tc_loss_ha_2001+tc_loss_ha_2002+tc_loss_ha_2003+tc_loss_ha_2004+tc_loss_ha_2005+tc_loss_ha_2006+tc_loss_ha_2007+tc_loss_ha_2008+tc_loss_ha_2009+tc_loss_ha_2010+tc_loss_ha_2011+tc_loss_ha_2012+tc_loss_ha_2013+tc_loss_ha_2014+tc_loss_ha_2015+tc_loss_ha_2016+tc_loss_ha_2017+tc_loss_ha_2018+tc_loss_ha_2019+tc_loss_ha_2020
   ) %>% 
   mutate(
     extent_2020_ha = extent_2000_ha + (gain_2000.2020_ha- loss_2000.2020_ha) ## If the forest was 10 in 2000, and then it gained 3 and then lost 5. It would be 10 + (3-5) = 10-2 = 8
   )
-
-head(tree_cover_loss_brazil)
-
 
 
 ## Combining Datasets
@@ -61,9 +60,6 @@ tree_cover_loss_brazil <- tree_cover_loss_brazil %>%
 mapping_BR <- tree_cover_loss_brazil %>%
   left_join(muni_sf, by = "name_muni") %>%
   rename(name_state = subnational1)
-
-head(mapping_BR)
-
 
 # Adjusting Extend ha 2000 and 2010 to be proportional to the sizes of the municipalities
 
@@ -94,7 +90,6 @@ mapping_BR  <-  mapping_BR  %>%
     TRUE ~ "Other" 
   ))
 
-head(mapping_BR)
 
 mapping_BR  <-  mapping_BR  %>% 
   mutate(actualChange = case_when(
@@ -132,14 +127,12 @@ custom_colors <- c("Large proportional gain" = "#138030", "Slight proportional g
 
 
 # data
-tree_cover_loss <- tree_cover_loss %>%
-  filter(country == "Brazil", threshold == 30) %>%
-  select(-country, -threshold) %>%
+mapping_BR_leanings <- mapping_BR %>%
   pivot_longer(
     cols      = starts_with("tc_loss_ha_"),
-    names_to  = "year",
+    names_to  = "year_tc",
     values_to = "loss_ha") %>%
-  mutate(year   = parse_number(year),
+  mutate(year_tc   = parse_number(year_tc),
          loss_k = loss_ha / 1000)
 
 # president / political leaning tibble
@@ -166,20 +159,35 @@ presidents <- tibble(
 
 pres_years <- presidents %>%
   rowwise() %>%
-  mutate(year = list(start:end)) %>%  
-  unnest(year) %>%
-  select(name, year, leaning)
+  mutate(year_tc = list(start:end)) %>%  
+  unnest(year_tc) %>%
+  select(name, year_tc, leaning) 
 
-
-annual_loss <- tree_cover_loss %>%
-  left_join(pres_years, by="year") %>%
-  group_by(year, name, leaning) %>%
+annual_loss <- mapping_BR_leanings %>%
+  left_join(pres_years, by= "year_tc") %>%
+  group_by(year_tc, name, leaning) %>%
   summarise(total_loss_k = sum(loss_k, na.rm=TRUE), .groups="drop")
 
-# rectangle moment
-ribbons <- pres_years %>% 
+ribbons <- pres_years %>%
   group_by(name, leaning) %>% 
-  summarize(xmin = min(year) - 0.5, xmax = max(year) + 0.5, .groups = "drop")
+  summarize(xmin = min(year_tc) - 0.5, xmax = max(year_tc) + 0.5, .groups = "drop")
+
+
+# Join all tc_loss_ha in one column
+data_long <- mapping_BR %>% 
+  select(name_state, name_muni, area_ha, starts_with("tc_loss_ha_"), geom) %>%
+  pivot_longer(cols = starts_with("tc_loss_ha_"),
+               names_to = "year_tc",
+               values_to = "loss_ha") %>%
+  mutate(year_tc = as.numeric(gsub("tc_loss_ha_", "", year_tc)),
+         loss_pct = (loss_ha / area_ha) * 100)
+
+# Summarize total loss per state per year
+state_loss <- data_long %>%
+  group_by(year_tc, name_state) %>% 
+  summarise(total_loss = sum(loss_ha, na.rm = TRUE), .groups = "drop") %>%
+  arrange(year_tc) %>%
+  mutate(roll_avg_3yr = rollmean(total_loss, k = 3, fill = NA, align = "right"))
 
 
 # Define UI for application that draws a histogram
@@ -216,7 +224,7 @@ ui <- page_navbar(
                
                fluidRow(
                  column(6, plotOutput("line_STATE_tree_cover_loss")),
-                 column(6, plotlyOutput("map_State_tcl_by_municipality"))
+                 column(6, plotOutput("map_State_tcl_by_municipality"))
                )
              )
            )
@@ -226,31 +234,14 @@ ui <- page_navbar(
 )
 
 
-# Join all tc_loss_ha in one column
-data_long <- mapping_BR %>% 
-  select(name_state, name_muni, area_ha, starts_with("tc_loss_ha_"), geom) %>%
-  pivot_longer(cols = starts_with("tc_loss_ha_"),
-               names_to = "year_tc",
-               values_to = "loss_ha") %>%
-  mutate(year_tc = as.numeric(gsub("tc_loss_ha_", "", year_tc)),
-         loss_pct = (loss_ha / area_ha) * 100)
-
-# Summarize total loss per state per year
-state_loss <- data_long %>%
-  group_by(year_tc, name_state) %>% 
-  summarise(total_loss = sum(loss_ha, na.rm = TRUE), .groups = "drop") %>%
-  arrange(year_tc) %>%
-  mutate(roll_avg_3yr = rollmean(total_loss, k = 3, fill = NA, align = "right"))
-
-
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
   output$line_COUNTRY_tree_cover_by_political_leaning_plot <- renderPlot({
     p <- ggplot() +
       geom_rect(data = ribbons, aes(xmin = xmin, xmax = xmax, ymin = -Inf, ymax = +Inf, fill = leaning), alpha = 0.2) +
-      geom_line(data = annual_loss, aes(x = year, y = total_loss_k), size = 0.8, color = "black") +
-      geom_point(data = annual_loss, aes(x = year, y = total_loss_k), color = "black", size = 2) +
+      geom_line(data = annual_loss, aes(x = year_tc, y = total_loss_k), size = 0.8, color = "black") +
+      geom_point(data = annual_loss, aes(x = year_tc, y = total_loss_k), color = "black", size = 2) +
       scale_fill_brewer(palette = "Set1", name = "Political Leaning") +
       labs(title    = "Annual tree cover loss, with political leaning", x = "Year", y = "Loss (×1000 ha)") +
       theme_minimal(base_size = 14) +
@@ -260,7 +251,7 @@ server <- function(input, output) {
     
   })
   
-  output$map_State_tcl_by_municipality <- renderPlotly({
+  output$map_State_tcl_by_municipality <- renderPlot({
       
       p <- ggplot() +
         geom_sf(data = mapping_BR, aes(fill = actualChange, geometry = geom),  lwd = 0.02) + 
